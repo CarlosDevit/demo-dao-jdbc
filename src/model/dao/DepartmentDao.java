327dd158036400bebfe9bb25d5ef0f90888a68b9
CarlosDevit <carlosdevitsantos@gmail.com>
2021-11-28 08:45:01 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.dao;
 
-/**
- *
- * @author carlo
- */
+import java.util.List;
+import model.entities.Department;
+
 public interface DepartmentDao {
-    
+
+    void insert(Department obj);
+
+    void update(Department obj);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
+
 }
