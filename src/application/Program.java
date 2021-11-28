42e3e3aeacbeac8a6d74b6d5763c30152aee05c0
CarlosDevit <carlosdevitsantos@gmail.com>
2021-11-28 10:40:45 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,10 +30,22 @@ public class Program {
             System.out.println(obj);
         });
 
-        System.out.println("\n=== TEST 4: seller insert ===");
+        /*System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg Grey", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("Inserted! New id = " + newSeller.getId());*/
+        
+        System.out.println("\n=== TEST 4: seller update ===");
+        seller = sellerDao.findById(8);
+        seller.setName("Damian Wayne");
+        seller.setEmail("damian@gmail.com");
+        seller.setBirthDate(new Date());
+        seller.setBaseSalary(3500.0);
+        sellerDao.update(seller);
+        
+        
+        System.out.println("Update Completed! New Data: \n" + seller);
+        
 
     }
 
